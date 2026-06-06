758a524841659b1e0af45626f2b04623ff6f1f23
Jagpreet Singh <jagpreet.singh2060@gmail.com>
2026-06-07 02:52:50 +0530

    streamlit setup + home screean

---
+++ src/components/footer.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def footer_home():
+    img_url = "https://i.ibb.co/YTYGn5qV/logo.png"
+    st.markdown(f"""
+                <div style = "
+                display : flex;
+                gap : 6px;
+                items-align: center;
+                justify-content : center;
+                margin-top : 2rem;"
+        <p style = "font-weight: bold;"> Created with ❤️ by JS</p>
+                </div>
+                """, unsafe_allow_html=True)
